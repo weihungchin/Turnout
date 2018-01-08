2a1e8b015e2519dbe72283ebc61c11deb653a641
WeiHung <whc1g10@gmail.com>
2018-01-08 22:58:30 +0800

    css

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,13 +1,15 @@
 
 <template>
 
-  <div class = "from-inline">
-    <h3>Sign In</h3>
+  <div class = "from-inline mx-3">
+    <h3 class = "text-center mt-5 mb-3">Sign In</h3>
     <div class="clearfix"></div>
-    <div class="form-group">
+    <div class="border border-primary mx-auto col-md-6 offset-md-3 py-3" >
+         <div class="">
+        <div class="form-group">
         <input type="text"
         placeholder="email"
-        class ="form-control"
+        class ="form-control mb-3"
         v-model = "email"
         >
         <input type="password" 
@@ -15,8 +17,16 @@
         placeholder="password"
         v-model = "password"
         >
+        </div>
     </div>
-    <button class="btn btn-primary" @click = "signIn">Sign In</button>
+    
+    <div class="text-center mt-10">
+         <button class="btn btn-primary btn-lg" @click = "signIn">Sign In</button>
+    </div>
+
+    </div>
+   
+   
     <br>
     <p>{{error.message}}</p>
     <br>
@@ -24,28 +34,27 @@
 </template>
 
 <script>
-    import { firebaseApp } from '../firebaseApp'
+import { firebaseApp } from "../firebaseApp";
 
-    export default{
-        data(){
-            return {
-                email:'',
-                password:'',
-                error:{
-                    message:''
-                }
-            }
-        },
-        methods:{
-            signIn(){
-                firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
-                .catch(
-                    error => {
-                        this.error = error
-                    }
-                )
-            }
-        }
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: {
+        message: ""
+      }
+    };
+  },
+  methods: {
+    signIn() {
+      firebaseApp
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .catch(error => {
+          this.error = error;
+        });
     }
-
+  }
+};
 </script>
